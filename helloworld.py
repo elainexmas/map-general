96d3709b363b2b93cc02f30964f50105153a2c3c
Elaine K. Lee <elee1225@gmail.com>
2014-06-27 21:13:50 -0500

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#from urllib2 import urlopen
+#from pattern import web
+
+name = raw_input("What is your name? ")
+zipcode = raw_input("What is your zip code? ")
+print "Hello,  "+name+".  You live in "+zipcode
+
+
